2ca7547d78e276d012aa5cfbad64a4af2054c2f3
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-07-08 16:32:28 +0500

    Item model filter has been added

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoReact.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DemoReact.Controllers
 {
-    public class ItemController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
     {
-        public IActionResult Index()
+        private static readonly IEnumerable<ItemModel> itemModels = new[]
         {
-            return View();
+            new ItemModel{Id =1, Title = "The Godfather", ImageId=1, Ranking=0,ItemType=1 },
+            new ItemModel{Id =2, Title = "Highlander", ImageId=2, Ranking=0,ItemType=1 },
+            new ItemModel{Id =3, Title = "Highlander II", ImageId=3, Ranking=0,ItemType=1 },
+            new ItemModel{Id =4, Title = "The Last of the Mohicans", ImageId=4, Ranking=0,ItemType=1 },
+            new ItemModel{Id =5, Title = "Police Academy 6", ImageId=5, Ranking=0,ItemType=1 },
+            new ItemModel{Id =6, Title = "Rear Window", ImageId=6, Ranking=0,ItemType=1 },
+            new ItemModel{Id =7, Title = "Road House", ImageId=7, Ranking=0,ItemType=1 },
+            new ItemModel{Id =8, Title = "The Shawshank Redemption", ImageId=8, Ranking=0,ItemType=1 },
+            new ItemModel{Id =9, Title = "Star Treck IV", ImageId=9, Ranking=0,ItemType=1 },
+            new ItemModel{Id =10, Title = "Superman 4", ImageId=10, Ranking=0,ItemType=1 },
+            new ItemModel{Id = 11, Title = "Abbey Road", ImageId=11, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 12, Title = "Adrenalize", ImageId=12, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 13, Title = "Back in Black", ImageId=13, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 14, Title = "Enjoy the Silence", ImageId=14, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 15, Title = "Parachutes", ImageId=15, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 16, Title = "Ride the Lightning", ImageId=16, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 17, Title = "Rock or Bust", ImageId=17, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 18, Title = "Rust in Peace", ImageId=18, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 19, Title = "St. Anger", ImageId=19, Ranking=0,ItemType=2 },
+            new ItemModel{Id = 20, Title = "The Final Countdown", ImageId=20, Ranking=0,ItemType=2 }
+
+        };
+
+        [HttpGet("{itemType:int}")]
+        public ItemModel[] Get(int itemType)
+        {
+            // iterate over all items list 
+            ItemModel[] items = (ItemModel[])itemModels.Where(i=> i.ItemType== itemType);
+            return items;
         }
     }
 }
